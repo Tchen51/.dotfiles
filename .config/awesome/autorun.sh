ecb0b0eb888b051fdb0e8734e47c66a4e1dd6811
Timothy Chen <timmy.chen77@gmail.com>
2020-10-03 18:39:27 -0700

    update

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,5 +7,17 @@ function run {
   fi
 }
 
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar pbar &
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload pbar &
+#  done
+#else
+#  polybar --reload pbar &
+#fi
+
 run nitrogen --restore
 run picom -b
